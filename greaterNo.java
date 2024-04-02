18b75d5a44354918d5397bd15783b497af6f2764
Kundan Kumar <rajkundan9535@gmail.com>
2024-04-02 19:10:40 +0530

    add new files

---
+++ greaterNo.java
@@ -0,0 +1,13 @@
+class greaterNo {
+    public static void main(String[] args) {
+        int a=12;
+        int b=22;
+        if (a>b) {
+            System.out.println("Greater number is "+a);
+        }
+        else
+        {
+            System.out.println("Greater number is "+b);
+        }
+    }
+}
